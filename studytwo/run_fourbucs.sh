c7e110e7957612e9283c483bc749ee153a2791b2
Damian Mingo Ndiwago damian.ndiwago@uni.lu <dmingo@access1.aion-cluster.uni.lux>
2023-10-23 16:55:39 +0200

    Run python script for fourbuckets

--- studytwo/run_fourbucs.sh
+++ studytwo/run_fourbucs.sh
@@ -17,6 +17,7 @@ echo "== Node list: ${SLURM_NODELIST}"
 echo "== Submit dir: ${SLURM_SUBMIT_DIR}"
 echo "== Number of tasks: ${SLURM_NTASKS}"
 
+
 module load lang/Python/3.8.6-GCCcore-10.2.0
 source $HOME/damian/damian-venv/bin/activate
 
