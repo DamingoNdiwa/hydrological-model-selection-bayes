49bcc6fb9e399422f371b20c65b0c3ba4598a121
Damian Mingo Ndiwago damian.ndiwago@uni.lu <dmingo@access1.aion-cluster.uni.lux>
2023-10-24 23:00:28 +0200

    Runs 15  python scripts for two  buckets model in parallel

--- studytwo/run_twobucs.sh
+++ studytwo/run_twobucs.sh
@@ -20,6 +20,7 @@ echo "== Number of tasks: ${SLURM_NTASKS}"
 module load lang/Python/3.8.6-GCCcore-10.2.0
 source $HOME/damian/damian-venv/bin/activate
 
+
 # TODO: On more modern GNU parallel this makes 01, 02, ..., 15
 parallel -j15 --plus python3 ./twobuckets.py --output_dir ./{0#} ::: {1..15}
 
