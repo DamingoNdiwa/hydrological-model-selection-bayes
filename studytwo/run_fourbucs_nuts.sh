ba51e500f47a37623fc7c3b3bef193b2dc5ba3bf
Damian Mingo Ndiwago damian.ndiwago@uni.lu <dmingo@access1.aion-cluster.uni.lux>
2023-10-25 09:22:26 +0200

    To run script for model with NUTS

--- studytwo/run_fourbucs_nuts.sh
+++ studytwo/run_fourbucs_nuts.sh
@@ -20,8 +20,7 @@ echo "== Number of tasks: ${SLURM_NTASKS}"
 
 module load lang/Python/3.8.6-GCCcore-10.2.0
 source $HOME/damian/damian-venv/bin/activate
-
-# TODO: On more modern GNU parallel this makes 01, 02, ..., 10
+0
 parallel -j1 --plus python3 ./fourbuckets_nuts.py  --output_dir ./{0#} ::: {1..1}
 
 
