b3339f2d54b4852b5cbde1c00d6b930fee03d47d
Damian Mingo Ndiwago damian.ndiwago@uni.lu <dmingo@access1.aion-cluster.uni.lux>
2024-10-24 10:09:15 +0200

    relative path fix

--- experiment_two/ppc/hpc_scripts/run_ppc3bucs.sh
+++ experiment_two/ppc/hpc_scripts/run_ppc3bucs.sh
@@ -20,5 +20,5 @@ echo "== Number of tasks: ${SLURM_NTASKS}"
 
 module load lang/Python/3.8.6-GCCcore-10.2.0
 source $HOME/damian/damian-venv/bin/activate
-python3 ppcstudytwo_3bucs.py
+python3 ../ppcstudytwo_3bucs.py
 conda deactivate
